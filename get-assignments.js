ce4ba3537010f2a7a3b91f06baea5ffec442fcb7
Simon Borer <simon.borer@gmail.com>
2018-11-27 21:49:34 -0500

    commiting because my computer crashed and that`s terrifying

--- get-assignments.js
+++ get-assignments.js
@@ -21,39 +21,44 @@ const assignmentArray = [];
 dirCont.forEach(folder => {
 
     // Create an object for each assignment
-    const fold = fs.readdirSync('assignments/' + folder);
-    const logIndex = fold.indexOf('logs');
-    if (logIndex > -1) {
-      fold.splice(logIndex, 1);
-    }
-    const assignmentObject = Object.create(null);
-    assignmentObject["assignment"] = folder;
-    assignmentObject["submissions"] = [];
-
-
-    // For each file in the assignment subfolder
-    fold.forEach(file => {
-
-        const filename = file.split('.').slice(0, -1).join('.');
-        const contents = fs.readFileSync('assignments/' + folder + '/' + file, 'utf8');
-        const questions = getQuestions(contents);
-
-        const submissionObject = Object.create(null);
-
-        submissionObject["filename"] = filename;
-        
-        if (filename == 'key') {
-            submissionObject['key'] = true;
-        } else {
-            submissionObject['key'] = false;
+    if (fs.lstatSync('./assignments/' + folder).isDirectory()) {
+        const fold = fs.readdirSync('assignments/' + folder);
+        const logIndex = fold.indexOf('logs');
+        if (logIndex > -1) {
+          fold.splice(logIndex, 1);
         }
+        const assignmentObject = Object.create(null);
+        assignmentObject["assignment"] = folder;
+        assignmentObject["submissions"] = [];
 
-        const outputObj = Object.assign(submissionObject, questions);
-        assignmentObject["submissions"].push(submissionObject);
 
-    });
+        // For each file in the assignment subfolder
+        fold.forEach(file => {
 
-    assignmentArray.push(assignmentObject);
+            if (fs.lstatSync('./assignments/' + folder + '/' + file).isFile()) {
+                const filename = file.split('.').slice(0, -1).join('.');
+                const contents = fs.readFileSync('assignments/' + folder + '/' + file, 'utf8');
+                const questions = getQuestions(contents);
+
+                const submissionObject = Object.create(null);
+
+                submissionObject["filename"] = filename;
+                
+                if (filename == 'key') {
+                    submissionObject['key'] = true;
+                } else {
+                    submissionObject['key'] = false;
+                }
+
+                const outputObj = Object.assign(submissionObject, questions);
+                assignmentObject["submissions"].push(submissionObject);
+                
+            }
+
+        });
+
+        assignmentArray.push(assignmentObject);
+    }
 
 });
 
--- puppet.js
+++ puppet.js
@@ -3,66 +3,80 @@ const puppeteer = require('puppeteer'),
     chalkAnimation = require('chalk-animation'),
     fs = require('fs-extra'),
     h2p = require('html2plaintext'),
-    StreamZip = require('node-stream-zip');
+    StreamZip = require('node-stream-zip'),
+    { promisify } = require('util'),
+    { resolve } = require('path');
+
+const readdir = promisify(fs.readdir),
+    stat = promisify(fs.stat);
+
+async function getFiles(dir) {
+    const subdirs = await readdir(dir);
+    const files = await Promise.all(subdirs.map(async (subdir) => {
+        const res = resolve(dir, subdir);
+        return (await stat(res)).isDirectory() ? getFiles(res) : res;
+    }));
+    return files.reduce((a, f) => a.concat(f), []);
+}
 
 const walking = chalk.bold.cyan.bgMagenta,
     error = chalk.bold.red;
 
 async function puppet(username, password, courseId, cvid) {
-console.log(walking('Launching'));
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+    // console.log(walking('Launching'));
+    // const browser = await puppeteer.launch();
+    // const page = await browser.newPage();
 
-const USERNAME_SELECTOR = '#username',
-    PASSWORD_SELECTOR = '#password',
-    BUTTON_SELECTOR = '#fm1 > section.row > input.btn-submit';
+    // const USERNAME_SELECTOR = '#username',
+    //     PASSWORD_SELECTOR = '#password',
+    //     BUTTON_SELECTOR = '#fm1 > section.row > input.btn-submit';
 
-console.log(walking('Logging in'));
+    // console.log(walking('Logging in'));
 
-await page.goto('https://login.humber.ca/cas/login');
+    // await page.goto('https://login.humber.ca/cas/login');
 
-console.log(walking('Submitting login info'));
+    // console.log(walking('Submitting login info'));
 
-await page.click(USERNAME_SELECTOR);
-await page.keyboard.type(username);
+    // await page.click(USERNAME_SELECTOR);
+    // await page.keyboard.type(username);
 
-await page.click(PASSWORD_SELECTOR);
-await page.keyboard.type(password);
+    // await page.click(PASSWORD_SELECTOR);
+    // await page.keyboard.type(password);
 
-await page.click(BUTTON_SELECTOR);
+    // await page.click(BUTTON_SELECTOR);
 
-console.log(walking('Login submitted'));
+    // console.log(walking('Login submitted'));
 
-let str = 'This is where it usually goes wrong';
-const rainbow = chalkAnimation.rainbow(str);
+    // let str = 'This is where it usually goes wrong';
+    // const rainbow = chalkAnimation.rainbow(str);
 
-setInterval(() => {
-    rainbow.replace(str += '.');
-}, 1000);
+    // setInterval(() => {
+    //     rainbow.replace(str += '.');
+    // }, 1000);
 
-try {
-    await page.waitForNavigation();
-} catch (error) {
-    chalkAnimation.neon(err);
-}
+    // try {
+    //     await page.waitForNavigation();
+    // } catch (error) {
+    //     chalkAnimation.neon(err);
+    // }
 
-console.log(walking('Redirect successful'));
+    // console.log(walking('Redirect successful'));
 
-await page.goto('https://learn.humber.ca/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1');
+    // await page.goto('https://learn.humber.ca/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1');
 
-console.log(walking('Navigating to grading page'));
+    // console.log(walking('Navigating to grading page'));
 
-await page.goto('https://learn.humber.ca/webapps/gradebook/do/instructor/viewNeedsGrading?sortDir=ASCENDING&sortCol=attemptDate&course_id=' + courseId + '&editPaging=true&numResults=200');
+    // await page.goto('https://learn.humber.ca/webapps/gradebook/do/instructor/viewNeedsGrading?sortDir=ASCENDING&sortCol=attemptDate&course_id=' + courseId + '&editPaging=true&numResults=200');
 
-console.log(walking('Setting viewport'));
+    // console.log(walking('Setting viewport'));
 
-await page.setViewport({ width: 9999, height: 9999 });
+    // await page.setViewport({ width: 9999, height: 9999 });
 
-console.log(walking('Waiting for page to fully load'));
+    // console.log(walking('Waiting for page to fully load'));
 
-await page.waitForSelector('#listContainer_databody');
+    // await page.waitForSelector('#listContainer_databody');
 
-const asnLinks = await page.evaluate(() => {
+    // const asnLinks = await page.evaluate(() => {
     //     const links = Array.from(document.querySelectorAll('#listContainer_databody > tr > th > a'));
     //     return links.map(link => {
     //         const linkObj = Object.create(null);
@@ -71,25 +85,25 @@ const asnLinks = await page.evaluate(() => {
     //         linkObj.student = link.textContent.trim().replace(/\s/g, "-").toLowerCase();
     //         return linkObj;
     //     });
-});
+    // });
 
-async function mkLinkDir(directory) {
-    try {
-        await fs.ensureDir(directory);
-    } catch (err) {
-        console.log(error(err));
-    }
-}
+    // async function mkLinkDir(directory) {
+    //     try {
+    //         await fs.ensureDir(directory);
+    //     } catch (err) {
+    //         console.log(error(err));
+    //     }
+    // }
 
-console.log(walking('Making log folder'));
+    // console.log(walking('Making log folder'));
 
-mkLinkDir('./assignments/logs');
+    // mkLinkDir('./assignments/logs');
 
-console.log(walking('Making log file'));
+    // console.log(walking('Making log file'));
 
-const logFile = fs.createWriteStream('./assignments/logs/log.txt');
+    // const logFile = fs.createWriteStream('./assignments/logs/log.txt');
 
-for (const link of asnLinks) {
+    // for (const link of asnLinks) {
 
     //     console.log(walking('Making ' + link.assn + ' directory'));
 
@@ -107,9 +121,6 @@ for (const link of asnLinks) {
     //         const dlLinks = Array.from(document.querySelectorAll('.dwnldBtn'));
     //         return dlLinks.map(dlLink => dlLink.getAttribute('href'));
     //     });
-
-    //     // 		renaming
-    //     // 		unzip
 
     //     if (downloadLinks.length) {
 
@@ -151,17 +162,17 @@ for (const link of asnLinks) {
     //     console.log(walking('Returning to grading page'));
     //     await page.goto('https://learn.humber.ca/webapps/gradebook/do/instructor/viewNeedsGrading?sortDir=ASCENDING&sortCol=attemptDate&course_id=' + courseId + '&editPaging=true&numResults=200');
 
-}
+    // }
 
-console.log(walking('Closing log file'));
+    // console.log(walking('Closing log file'));
 
-logFile.end();
+    // logFile.end();
 
-console.log(walking('Closing browser'));
+    // console.log(walking('Closing browser'));
 
-await browser.close();
+    // await browser.close();
 
-console.log(walking('Listing subdirectories in assignments'));
+    console.log(walking('Listing subdirectories in assignments'));
 
     const dirCont = fs.readdirSync('assignments');
 
@@ -171,17 +182,17 @@ console.log(walking('Listing subdirectories in assignments'));
         const subfolder = fs.readdirSync('assignments/' + folder);
 
         subfolder.forEach(file => {
-        	console.log(file);
+            console.log(file);
             const fileExt = file.substring(file.length - 4),
-            	filePath = 'assignments/' + folder + '/';
+                filePath = 'assignments/' + folder + '/';
             switch (fileExt) {
                 case '.txt':
                     const newName = file.replace('.txt', '.sql');
                     console.log(walking('Renaming ' + file + ' to ' + newName));
                     try {
-	                    fs.rename(filePath + file, filePath + newName);                	
+                        fs.rename(filePath + file, filePath + newName);
                     } catch (error) {
-                    	console.log(error);
+                        console.log(error);
                     }
                     break;
                 case '.zip':
@@ -202,11 +213,71 @@ console.log(walking('Listing subdirectories in assignments'));
                     } catch (error) {
                         chalkAnimation.neon(error);
                     }
+
                     break;
+
             }
         })
 
     });
+
+    dirCont.forEach(folder => {
+
+    	const subfolder = fs.readdirSync('./assignments/' + folder).filter(sbfolder => fs.statSync('./assignments/' + folder + '/' + sbfolder).isDirectory());
+
+    	subfolder.forEach(innerFolders => {
+    		const kidFolders = fs.readdirSync('./assignments/' + folder + '/' + innerFolders);
+    		kidFolders.forEach(kf => {
+    			const kfFilePath = './assignments/' + folder + '/' + innerFolders + '/';
+    			if (fs.statSync(kfFilePath + kf).isDirectory()) {
+	    			getFiles(kfFilePath + kf)
+					  .then(function(value) {
+					  	value.forEach(val => {
+					  		if (val.substring(val.length - 9) = '.DS_Store') {
+					  			fs.unlink(val);
+					  			console.log(walking('Deleted .DS_Store file'))
+					  		} else {
+					  			  try {
+					  			  	const dstpath = './assignments/' + folder + val.split("/").pop();
+								    await fs.move(val, dstpath)
+								    console.log(walking('Moved ' + val.split("/").pop() + ' from ' + val + ' to ./assignments/' + folder));
+								  } catch (err) {
+								    console.error(err)
+								  }
+					  		}
+					  	})
+					  })
+					  .catch(e => console.error(e));
+					}
+    			// switch (kf) {
+    			// 	case '.DS_Store':
+    			// 		fs.unlink(kfFilePath + kf);
+
+
+    			// }
+    		});
+    		
+    		// console.log(error(innerFolders))
+    		// innerFolders.forEach(innerFolder => {
+    		// 	console.log(innerFolder)
+    		// })
+    		// const filePath = 'assignments/' + folder + '/';
+		    // const kidFolder = fs.readdirSync('./' + filePath + file);
+    		// console.log(error(kidFolder));
+
+		    // kidFolder.forEach(folder => {
+		    // 	const path = filePath + folder;
+		    // 	console.log(error("is " + path + " a folder?"));
+		    //     if (fs.statSync(path).isDirectory()) {
+		    //         getFiles(filePath + '/' + folder)
+		    //             .then(files => { console.log('filepath: ' + filePath + '/' + folder + ', files ' + files) })
+		    //             .catch(e => console.error(e));
+
+		    //     }
+		    // })
+    		
+    	})
+    })
 }
 
 module.exports = puppet;
